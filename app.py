ee47e70ae3dbf29d6087d5c6b17b3841cbffc370
Iniyan <viniyan563@gmail.com>
2021-10-09 15:47:58 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return render_template('index.html')
 @app.route('/greet', methods = ['POST','GET'])
 def greet():
-    flash("Hi " + str(request.form['name_input']) + " ,great to see you!")
+    flash("Hi " + str(request.form['name_input']) + " ,great to see you !")
     return render_template('index.html')
 if __name__ == "__main__":
     app.run(debug=True)
